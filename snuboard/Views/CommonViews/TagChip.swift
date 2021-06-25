7af55f97673b5ee0c36ef5541c698c142c2ca7bf
Subeen Park <psb021414@gmail.com>
2021-06-26 03:27:44 +0900

    [add] tagchips

--- snuboard/Views/CommonViews/TagChip.swift
+++ snuboard/Views/CommonViews/TagChip.swift
@@ -7,14 +7,54 @@
 
 import SwiftUI
 
-struct TagChip: View {
+struct TagChipView: View {
+    
+    
+    let item: String
+    @State var selected = false
+    let actionIfSelected: () -> Void
+    let actionIfUnSelected: () -> Void
+    
+    
+    init (item: String, selected: Bool, actionIfSelected: @escaping ()-> Void, actionIfUnselected: @escaping ()->Void) {
+        self.item = item
+        self.selected = selected
+        self.actionIfSelected = actionIfSelected
+        self.actionIfUnSelected = actionIfUnselected
+    }
+ 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        Text(verbatim: item)
+            .font(.system(size: 10))
+            .foregroundColor(.white)
+            .bold()
+            .padding(5)
+            .padding(.leading, 4)
+            .padding(.trailing, 4)
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                .fill(selected ? Const.ColorSet.MainBlue.color : Const.ColorSet.Unselected.color)
+            )
+            .onTapGesture {
+                selected.toggle()
+                if selected {
+                    actionIfSelected()
+                }
+                else {
+                    actionIfUnSelected()
+                }
+            }
+        
     }
 }
 
-struct TagChip_Previews: PreviewProvider {
+struct TagChipView_Previews: PreviewProvider {
     static var previews: some View {
-        TagChip()
+        TagChipView(item: "입학", selected: false, actionIfSelected: {
+            print("action")
+        }, actionIfUnselected: {
+            
+        })
     }
 }
