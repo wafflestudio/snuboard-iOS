4d2c0e5db1c64630baf75abc6b0a9f1fc87eeb29
Subeen Park <psb021414@gmail.com>
2021-06-26 03:24:51 +0900

    [add] NoticeAPI/NoticeService

--- snuboard/Networking/API/NoticeAPI.swift
+++ snuboard/Networking/API/NoticeAPI.swift
@@ -5,4 +5,85 @@
 //  Created by Subeen Park on 2021/06/25.
 //
 
+
 import Foundation
+import Alamofire
+
+enum NoticeAPI: BaseAPI {
+    
+    static var baseURL: String = "https://snuboard-api.wafflestudio.com/notices/"
+
+    
+    var accessTokenNeeded: Bool {
+        return true
+    }
+    
+    var refreshTokenNeeded: Bool {
+        return false
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getNoticesByDepartmentId:
+            return .get
+        default:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getNoticesByDepartmentId(let id):
+            return "department/\(id)"
+        default:
+            return ""
+        }
+    }
+    
+    var param: [String : Any]? {
+        switch self {
+        
+        case .getNoticesByDepartmentId:
+            return ["limit": 30]
+    
+        
+        default:
+            return nil
+        }
+        
+        
+    }
+    
+    
+    case getNoticesByDepartmentId(id: Int)
+    
+    
+    
+    func requestAPI() -> DataRequest {
+        
+        let URL = Self.baseURL + path
+        
+        var header: HTTPHeaders = ["Content-Type": "application/json"]
+        
+        if accessTokenNeeded {
+            header["Authorization"] = TokenUtils.getAccessTokenHeader()
+            
+        }
+        
+        if refreshTokenNeeded {
+            header["Authorization"] = TokenUtils.getRefreshTokenHeader()
+            
+        }
+        
+        return AF.request(URL,
+                          method: method,
+                          parameters: param,
+                          encoding: URLEncoding.queryString,
+                          headers: header
+                          )
+        
+    }
+    
+    
+    
+}
--- snuboard/Networking/NoticeService.swift
+++ snuboard/Networking/NoticeService.swift
@@ -5,4 +5,48 @@
 //  Created by Subeen Park on 2021/06/25.
 //
 
+
 import Foundation
+import Alamofire
+
+
+struct NoticeService {
+    
+    static let shared = NoticeService()
+    
+    public func getNoticesByDepartmentId(id: Int, completion: @escaping (NetworkResult<NoticeSummaryListDataModel>) -> ()) {
+        
+        let dataRequest = NoticeAPI.getNoticesByDepartmentId(id: id).requestAPI()
+        
+        dataRequest.responseData { dataResponse in
+            
+            switch dataResponse.result {
+
+            // success status code
+            case .success:
+                guard let statusCode = dataResponse.response?.statusCode
+                else {return}
+                print(statusCode)
+                guard let value = dataResponse.value else {return}
+                print(value)
+                
+                let networkResult: NetworkResult<NoticeSummaryListDataModel> = NetworkResult<Any>.judgeStatus(by: statusCode, data: value)
+
+                completion(networkResult)
+
+            // faiulure status code
+            case .failure: completion(.pathError)
+
+
+            }
+            
+        }
+
+    
+    } // end of getNoticesByDepartmentId
+    
+    
+   
+}
+
+
