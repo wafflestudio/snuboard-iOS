630f386175b8a046b3c9bdc83f62a0c7efbce579
Subeen Park <psb021414@gmail.com>
2021-07-09 18:24:51 +0900

    [fix] add notice overwritten NoticeViewModel

--- snuboard/ViewModel/NoticeViewModel.swift
+++ snuboard/ViewModel/NoticeViewModel.swift
@@ -1,12 +1,147 @@
 //
-//  NoticeModel.swift
+//  NoticeViewModel.swift
 //  snuboard
 //
-//  Created by Subeen Park on 2021/07/09.
+//  Created by Subeen Park on 2021/06/25.
 //
 
-import SwiftUI
+import Foundation
+import Combine
 
-protocol NoticeModel {
-    var notices: [NoticeSummary] { get set }
+class NoticeViewModel: ObservableObject {
+    
+    @Published var notices: [NoticeSummary] = []
+    @Published var nextCursor: String = ""
+    
+    init(id: Int) {
+        NoticeService.shared.getNoticesByDepartmentId(id: id) { response in
+
+            switch response {
+
+            case .success(let noticeData):
+
+                self.notices = noticeData.notices
+                self.nextCursor = noticeData.nextCursor
+                
+                print("noticesPatched")
+
+            case .badRequest(let badRequest):
+                print("badRequest: \(badRequest.message)")
+                
+            case .unauthorized(let unautorized):
+                print("unautorized: \(unautorized.message)")
+
+            default:
+                print("Other networking error")
+            }
+
+
+        }
+    }
+    
+    init() {
+        NoticeService.shared.getScrappedNotices { response in
+
+                switch response {
+
+                case .success(let noticeData):
+
+                    self.notices = noticeData.notices
+                    self.nextCursor = noticeData.nextCursor
+                    
+                    print("noticesPatched")
+
+                case .badRequest(let badRequest):
+                    print("badRequest: \(badRequest.message)")
+                    
+                case .unauthorized(let unautorized):
+                    print("unautorized: \(unautorized.message)")
+
+                default:
+                    print("Other networking error")
+                }
+        }
+    }
+    
+    
+    func getAllNoticesScrapped() {
+        NoticeService.shared.getScrappedNotices { response in
+
+                switch response {
+
+                case .success(let noticeData):
+
+                    self.notices = noticeData.notices
+                    self.nextCursor = noticeData.nextCursor
+                    
+                    print("noticesPatched")
+
+                case .badRequest(let badRequest):
+                    print("badRequest: \(badRequest.message)")
+                    
+                case .unauthorized(let unautorized):
+                    print("unautorized: \(unautorized.message)")
+
+                default:
+                    print("Other networking error")
+                }
+        }
+        
+    }
+    
+    
+    func postNoticeScrap(id: Int) {
+        
+        NoticeService.shared.postNoticeScrap(id: id) { response in
+
+            switch response {
+
+            case .success(let data):
+            
+                if let noticeIdx = self.notices.firstIndex(where: { $0.id == data.id }) {
+                    self.notices[noticeIdx] = data
+                        print("\(data.id)-scrapped!")
+                    }
+
+            case .badRequest(let badRequest):
+                print("badRequest: \(badRequest.message)")
+                
+            case .unauthorized(let unautorized):
+                print("unautorized: \(unautorized.message)")
+
+            default:
+                print("Other networking error")
+            }
+
+    
+        }
+        
+    }
+    
+    func deleteNoticeScrap(id: Int) {
+        
+        NoticeService.shared.deleteNoticeScrap(id: id) { response in
+
+            switch response {
+
+            case .success(let data):
+                self.notices = self.notices.filter({ $0.id != data.id})
+
+            case .badRequest(let badRequest):
+                print("badRequest: \(badRequest.message)")
+                
+            case .unauthorized(let unautorized):
+                print("unautorized: \(unautorized.message)")
+
+            default:
+                print("Other networking error")
+            }
+
+    
+        }
+        
+    }
+    
+   
+    
 }
