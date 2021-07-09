6122db92b714d72434f98e65ccd983400fe63f4a
Subeen Park <psb021414@gmail.com>
2021-07-09 14:31:09 +0900

    bug: networkFailure

---
+++ snuboard/ViewModel/NoticeDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoticeDetailViewModel.swift
+//  snuboard
+//
+//  Created by Subeen Park on 2021/07/09.
+//
+
+import Foundation
---
+++ snuboard/Views/CommonViews/NoticeDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  NoticeDetailView.swift
+//  snuboard
+//
+//  Created by Subeen Park on 2021/07/09.
+//
+
+import Foundation
