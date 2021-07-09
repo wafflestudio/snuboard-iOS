716bd0dc49dad2746b310698db3403e9999f5491
Subeen Park <psb021414@gmail.com>
2021-07-09 13:02:50 +0900

    [add] NoticeDetail

---
+++ snuboard/ViewModel/NoticeDetailViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  NoticeDetailViewModel.swift
+//  snuboard
+//
+//  Created by Subeen Park on 2021/07/09.
+//
+
+import SwiftUI
+
+struct NoticeDetailViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoticeDetailViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        NoticeDetailViewModel()
+    }
+}
---
+++ snuboard/Views/CommonViews/NoticeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  NoticeDetailView.swift
+//  snuboard
+//
+//  Created by Subeen Park on 2021/07/09.
+//
+
+import SwiftUI
+
+struct NoticeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoticeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoticeDetailView()
+    }
+}
