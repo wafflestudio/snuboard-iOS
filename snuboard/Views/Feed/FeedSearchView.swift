dccedbb1d18ede00f393d5a1872c3c201891369d
Subeen Park <psb021414@gmail.com>
2021-07-16 23:58:14 +0900

    fix: search boolean modify

---
+++ snuboard/Views/Feed/FeedSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedSearchView.swift
+//  snuboard
+//
+//  Created by Subeen Park on 2021/07/16.
+//
+
+import SwiftUI
+
+struct FeedSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedSearchView()
+    }
+}
