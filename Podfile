d5a5c11db950f8c5d3e7b3a085097fc52a79bc80
Subeen Park <psb021414@gmail.com>
2021-08-31 00:33:12 +0900

    feat: push notifications

--- Podfile
+++ Podfile
@@ -10,7 +10,8 @@ target 'snuboard' do
   pod 'SnapKit', '~> 5.0.0'
   pod 'Moya', '~> 14.0'
   pod 'Alamofire', '~> 5.2'
-
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Messaging'
 
   target 'snuboardTests' do
     inherit! :search_paths
---
+++ snuboard/Sources/ViewModifiers/ImageModifiers.swift
@@ -0,0 +1,20 @@
+//
+//  ImageModifiers.swift
+//  snuboard
+//
+//  Created by Subeen Park on 2021/08/31.
+//
+
+import SwiftUI
+
+struct ImageModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageModifiers_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageModifiers()
+    }
+}
