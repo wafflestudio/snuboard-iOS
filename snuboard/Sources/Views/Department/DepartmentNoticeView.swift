56b8caa4575a67f73d9265e49568bd4ff8a68620
Subeen Park <psb021414@gmail.com>
2021-11-15 20:12:41 +0900

    fix department notice initialization bug

--- snuboard/Sources/Views/Department/DepartmentNoticeView.swift
+++ snuboard/Sources/Views/Department/DepartmentNoticeView.swift
@@ -18,78 +18,84 @@ struct DepartmentNoticeView: View {
     
     var body: some View {
         
-        ScrollView {
+        ZStack {
             
-            VStack(spacing: 0) { // VSTACK 0
+            ScrollView {
                 
-                TagFilterView(searchText: $searchText)
-                    .environmentObject(deptModel)
-                    .environmentObject(settings)
-                    .padding(12)
-                    .background(RoundedRectangle(cornerRadius: 8).foregroundColor(.white))
-                    .padding([.top, .leading, .trailing], 10)
+                VStack(spacing: 0) { // VSTACK 0
+                    
+                    TagFilterView(searchText: $searchText)
+                        .environmentObject(deptModel)
+                        .environmentObject(settings)
+                        .padding(12)
+                        .background(RoundedRectangle(cornerRadius: 8).foregroundColor(.white))
+                        .padding([.top, .leading, .trailing], 10)
+                    
+                    if !deptModel.notices.isEmpty || deptModel.nextCursor != "" || deptModel.loading {
 
-                if deptModel.notices.isEmpty && deptModel.nextCursor == "" && !deptModel.loading {
-                    VStack {
-                        Spacer()
-                        Image("search_placeholder")
-                            .resizable()
-                            .frame(width: 203, height: 203, alignment: .center)
+                            VStack {
+                                ForEach(deptModel.notices) { noticeSummary in
+                                    NavigationLink(destination: DeptNoticeDetailView(id: noticeSummary.id), label: {
+                                        DeptNoticeSummaryView(notice: noticeSummary)
+                                    })
+                                }
+                                if !deptModel.noMoreContent && !deptModel.loading {
+                                    HStack { // Load more Hstack
+                                        Spacer()
+                                        HStack {
+                                            Button(action: {
+                                                
+                                                if searchText.count < 2 {
+                                                    print("load ordinary notices")
+                                                    deptModel.loadMoreNoticesByDepartmentId(tags: UserDefaults.standard.queryParameters[deptModel.department.name] ?? [])
+                                                } else {
+                                                    print("load search results")
+                                                    deptModel.loadMoreSearchResultsByDepartmentId(tags: UserDefaults.standard.queryParameters[deptModel.department.name] ?? [], keywords: searchText)
+                                                }
+                                                
+                                            }, label: {
+                                                Text("더보기")
+                                                    .foregroundColor(Const.Colors.MainBlue.color)
+                                                    .font(.system(size: 14))
+                                            })
+                                            
+                                        }
+                                        Spacer()
+                                    } // end of load more Hstack
+                                }
+                                
+                            }.padding(10)
+                            if deptModel.loading {
+                                HStack {
+                                    Spacer()
+                                    ProgressView()
+                                        .progressViewStyle(CircularProgressViewStyle())
+                                    Spacer()
+                                }
+                            }
+                        
+                        
                     }
-                }
-                
-                else {
-                    
-                    ScrollView {
-                       
-                        VStack {
-                            ForEach(deptModel.notices) { noticeSummary in
-                                NavigationLink(destination: DeptNoticeDetailView(id: noticeSummary.id), label: {
-                                    DeptNoticeSummaryView(notice: noticeSummary)
-                                })
-                            }
-                            if !deptModel.noMoreContent && !deptModel.loading {
-                                HStack { // Load more Hstack
-                                    Spacer()
-                                    HStack {
-                                        Button(action: {
-                                            
-                                            if searchText.count < 2 {
-                                                print("load ordinary notices")
-                                                deptModel.loadMoreNoticesByDepartmentId(tags: UserDefaults.standard.queryParameters[deptModel.department.name] ?? [])
-                                            } else {
-                                                print("load search results")
-                                                deptModel.loadMoreSearchResultsByDepartmentId(tags: UserDefaults.standard.queryParameters[deptModel.department.name] ?? [], keywords: searchText)
-                                            }
-                                            
-                                        }, label: {
-                                            Text("더보기")
-                                                .foregroundColor(Const.Colors.MainBlue.color)
-                                                .font(.system(size: 14))
-                                        })
-                                        
-                                    }
-                                    Spacer()
-                                } // end of load more Hstack
-                            }
-                            
-                        }.padding(10)
-                        if deptModel.loading {
-                            HStack {
-                                Spacer()
-                                ProgressView()
-                                    .progressViewStyle(CircularProgressViewStyle())
-                                Spacer()
-                            }
-                        }
-                    } // End of scroll view
-                    .padding(.top, 1)
-                    
-                }
 
-            } // END OF VSTACK 0
-            .background(Const.Colors.BgGray.color)
+                } // END OF VSTACK 0
+                
+            } // END OF SCROLL VIEW
+            
+            
+            if deptModel.notices.isEmpty && deptModel.nextCursor == "" && !deptModel.loading {
+                VStack {
+                    Spacer()
+                    Image("search_placeholder")
+                        .resizable()
+                        .frame(width: 203, height: 203, alignment: .center)
+                }
+            }
+
+            
         }
+        .background(Const.Colors.BgGray.color)
+        .padding(.top, 1)
+
     }
 }
 
